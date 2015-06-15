e2f428ec7665fb6ef78e5d88ad9d0d2b5dd3aa52
Giorgi <giorgirokhadze@gmail.com>
2015-06-15 17:05:18 +0400

    Finished

--- src/main/java/ge/edu/freeuni/sdp/xo/game/GameService.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/game/GameService.java
@@ -28,64 +28,71 @@ import javax.ws.rs.core.Response.Status;
 @Consumes({ MediaType.APPLICATION_JSON })
 @Produces({ MediaType.APPLICATION_JSON })
 public class GameService {
-    
+
 	static final String GAME_EMPTY = "EmptyGame";
 	static final String GAME_RUNNING = "RunningGame";
 	static final String GAME_FINISHED = "FinishedGame";
-	
+
 	static final String GAME_CLOSED = "ClosedGame";
 	static final String GAME_OPENED = "OpenedGame";
-	
-    static final HashMap<String, GameState> games = new HashMap<>();
-    static{
-        GameState state;
-        state = new GameState();
-        state.status = GameState.STATUS_PENDING;
-        games.put("EmptyGame", state);
-     
-        state = new GameState();
-        state.status = GameState.STATUS_RUNNING;
-        state.table = Arrays.asList(16, 1, 2);
-        games.put("RunningGame", state);
-        
-        state = new GameState();
-        state.status = GameState.STATUS_FINISHED;
-        state.table = Arrays.asList(16, 1, 4, 64, 32, 8, 256);
-        state.winner = "x-player_id";
-        games.put("FinishedGame", state);
-    }
 
-    @GET
-    public GameState getState(@PathParam("gameID") String gameID){
-        GameState state = games.get(gameID);
-        if(state == null)
-            throw new WebApplicationException(Status.NOT_FOUND);
-        return state;
-    }
-    
-    @POST
-    public Response registerPlayer(@PathParam("gameID") String gameID, JsonRequest request){
-        if(request.user_id == null) return Response.status(Status.BAD_REQUEST).build();
-        System.out.println(request);
-        System.out.println("game: "+gameID);
-        switch(gameID){
-            case "ClosedGame": return Response.status(Status.CONFLICT).build();
-            case "OpenedGame": return Response.status(Status.OK).build();
-            default: return Response.status(Status.NOT_FOUND).build();
-        }
-        
-    }
-    
-    @PUT
-    public GameState makeMove(@PathParam("gameID") String gameID, JsonRequest request){
-        System.out.println("user: "+request.user_id + ", cell: "+request.cell);
-        if(request.user_id == null || request.cell == 0)
-            throw new WebApplicationException(Status.BAD_REQUEST);
-        
-        GameState state = games.get(gameID);
-        if(state == null)
-            throw new WebApplicationException(Status.NOT_FOUND);
-        
-        return state;
-    }
+	static final HashMap<String, GameState> games = new HashMap<>();
+	static {
+		GameState state;
+		state = new GameState();
+		state.status = GameState.STATUS_PENDING;
+		games.put(GAME_EMPTY, state);
+
+		state = new GameState();
+		state.status = GameState.STATUS_RUNNING;
+		state.table = Arrays.asList(16, 1, 2);
+		games.put(GAME_RUNNING, state);
+
+		state = new GameState();
+		state.status = GameState.STATUS_FINISHED;
+		state.table = Arrays.asList(16, 1, 4, 64, 32, 8, 256);
+		state.winner = "x-player_id";
+		games.put(GAME_FINISHED, state);
+	}
+
+	@GET
+	public GameState getState(@PathParam("gameID") String gameID) {
+		GameState state = games.get(gameID);
+		if (state == null)
+			throw new WebApplicationException(Status.NOT_FOUND);
+		return state;
+	}
+
+	@POST
+	public Response registerPlayer(@PathParam("gameID") String gameID,
+			JsonRequest request) {
+		if (request.user_id == null)
+			return Response.status(Status.BAD_REQUEST).build();
+		System.out.println(request);
+		System.out.println("game: " + gameID);
+		switch (gameID) {
+		case GAME_CLOSED:
+			return Response.status(Status.CONFLICT).build();
+		case GAME_OPENED:
+			return Response.status(Status.OK).build();
+		default:
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+	}
+
+	@PUT
+	public GameState makeMove(@PathParam("gameID") String gameID,
+			JsonRequest request) {
+		System.out.println("user: " + request.user_id + ", cell: "
+				+ request.cell);
+		if (request.user_id == null || request.cell == 0)
+			throw new WebApplicationException(Status.BAD_REQUEST);
+
+		GameState state = games.get(gameID);
+		if (state == null)
+			throw new WebApplicationException(Status.NOT_FOUND);
+
+		return state;
+	}
 }
--- test/ge/edu/freeuni/sdp/xo/game/GameServiceTest.java
+++ test/ge/edu/freeuni/sdp/xo/game/GameServiceTest.java
@@ -1,31 +1,128 @@
 package ge.edu.freeuni.sdp.xo.game;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
-public class GameServiceTest {
+public class GameServiceTest extends JerseyTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(GameService.class);
 	}
 
 	@Test
-	public void testGetState() {
+	public void testGetStatusOk() {
+		Response actual = target(GameService.GAME_EMPTY).request().get();
+		assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
 	}
 
 	@Test
-	public void testRegisterPlayer() {
+	public void testGetStatusError() {
+		Response actual = target("kasgdh").request().get();
+		assertEquals(Response.Status.NOT_FOUND.getStatusCode(),
+				actual.getStatus());
 	}
 
 	@Test
-	public void testMakeMove() {
+	public void testGetResponseOk() {
+		Response actual = target(GameService.GAME_EMPTY).request().get();
+		assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
+		// TODO: Response needs to be checked!
+	}
+
+	@Test
+	public void testPutStatusOk() {
+		JsonRequest jr = new JsonRequest();
+		jr.cell = 16;
+		jr.user_id = "me";
+		Response actual = target(GameService.GAME_EMPTY).request().put(
+				Entity.json(jr));
+		assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
+	}
+
+	@Test
+	public void testPutStatusNotFound() {
+		JsonRequest jr = new JsonRequest();
+		jr.user_id = "me";
+		jr.cell = 16;
+		Response actual = target("ads").request().put(Entity.json(jr));
+		assertEquals(Response.Status.NOT_FOUND.getStatusCode(),
+				actual.getStatus());
+	}
+
+	@Test
+	public void testPutStatusBadRequest() {
+		JsonRequest jr = new JsonRequest();
+		Response actual = target("ads").request().put(Entity.json(jr));
+		assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+				actual.getStatus());
+	}
+
+	@Test
+	public void testPutStatusBadRequestNull() {
+		JsonRequest jr = new JsonRequest();
+		jr.user_id = "me";
+		jr.cell = 0;
+		Response actual = target("ads").request().put(Entity.json(jr));
+		assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+				actual.getStatus());
+	}
+
+	@Test
+	public void testPostStatusOk() {
+		JsonRequest jr = new JsonRequest();
+		jr.user_id = "me";
+		jr.cell = 0;
+		Response actual = target(GameService.GAME_OPENED).request().post(
+				Entity.json(jr));
+		assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
+	}
+
+	@Test
+	public void testPostStatusConflict() {
+		JsonRequest jr = new JsonRequest();
+		jr.user_id = "me";
+		jr.cell = 0;
+		Response actual = target(GameService.GAME_CLOSED).request().post(
+				Entity.json(jr));
+		assertEquals(Response.Status.CONFLICT.getStatusCode(),
+				actual.getStatus());
+	}
+
+	@Test
+	public void testPostStatusNotFound() {
+		JsonRequest jr = new JsonRequest();
+		jr.user_id = "me";
+		jr.cell = 0;
+		Response actual = target("dsfghjk").request().post(Entity.json(jr));
+		assertEquals(Response.Status.NOT_FOUND.getStatusCode(),
+				actual.getStatus());
+	}
+
+	@Test
+	public void testPostStatusBadRequst() {
+		JsonRequest jr = new JsonRequest();
+		Response actual = target(GameService.GAME_OPENED).request().post(
+				Entity.json(jr));
+		assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+				actual.getStatus());
+	}
+
+	@Test
+	public void testPostStatusEmpty() {
+		JsonRequest jr = new JsonRequest();
+		jr.user_id = "me";
+		Response actual = target(GameService.GAME_EMPTY).request().post(
+				Entity.json(jr));
+		assertEquals(Response.Status.NOT_FOUND.getStatusCode(),
+				actual.getStatus());
 	}
 
 }
