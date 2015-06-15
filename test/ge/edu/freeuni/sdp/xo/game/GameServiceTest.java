a8b050937549880e4850ab73789b19fcc837795f
Giorgi <giorgirokhadze@gmail.com>
2015-06-15 11:16:46 +0400

    Create GameServiceTest

---
+++ test/ge/edu/freeuni/sdp/xo/game/GameServiceTest.java
@@ -0,0 +1,34 @@
+package ge.edu.freeuni.sdp.xo.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameServiceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetState() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterPlayer() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeMove() {
+//		fail("Not yet implemented");
+	}
+
+}
